118f2e527134ed112c0f74e5e6850a371655c44c
nakedsend-bit <nakedsend@gmail.com>
2026-03-06 19:46:42 -0300

    Create Firecrawl service class for extracting and saving product data

---
+++ src/services/firecrawl.service.ts
@@ -0,0 +1,14 @@
+// firecrawl.service.ts
+
+class FirecrawlService {
+    extractProductData(html: string) {
+        // logic to extract product data from HTML
+        return productData;
+    }
+
+    saveProductData(productData: object) {
+        // logic to save product data to database
+    }
+}
+
+export default FirecrawlService;
